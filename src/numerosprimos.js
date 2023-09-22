4df78de9fd8267737633acb0f7fbb634f5d47cba
wilnermena <wi_an9@hotmail.com>
2023-09-21 23:55:20 -0400

    Debería retornar una lista  para [3,3]

--- src/numerosprimos.js
+++ src/numerosprimos.js
@@ -3,19 +3,20 @@ class primos{
   factorprimo(n){
     
     let resulprimos=[];
+    let cantidad=2;
+    while (n>1) {
 
-    if (n>1)
-    {
-      while(n%2 === 0){
-        resulprimos.push(2);
-        n/=2;
+       while(n%cantidad === 0){
+
+        resulprimos.push(cantidad);
+        n/=cantidad;
       }
+      cantidad ++;
+    }
       if(n>1)
       {
         resulprimos.push(n);
       }
-      
-    }
 
     return resulprimos;
 
